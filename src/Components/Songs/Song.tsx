af7a8791b9c0052db3523d4cab623557ca537831
christian4747 <christianan711+2@gmail.com>
2024-11-15 13:27:30 -0500

    Finished skeleton and functionality for working Song endpoints

--- src/Components/Songs/Song.tsx
+++ src/Components/Songs/Song.tsx
@@ -1,4 +1,7 @@
-import { Container } from "react-bootstrap"
+import axios from "axios";
+import { useState } from "react";
+import { Button, Dropdown, DropdownButton, Form, Modal } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
 
 type SongInfo = {
     songId: number,
@@ -8,16 +11,77 @@ type SongInfo = {
 }
 
 export const Song: React.FC<{song: SongInfo}> = ({song}) => {
-    
+
+    // Song information
     const songInfo = song
 
+    // State for hiding if song is deleted
+    const [hidden, setHidden] = useState(false)
+
+    // State for showing/hiding the "Add to Playlist" modal
+    const [show, setShow] = useState(false);
+
+    // Functions for handling opening and closing the modal
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // Function for handling a song deletion
+    const handleDelete = async () => {
+
+        if (window.confirm("Delete song with ID: " + song.songId + "?")) {
+            await axios.delete("http://localhost:8080/songs/" + song.songId)
+            .then(() => {
+                setHidden(true)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+        }
+
+    }
+
     return (
-        <Container>
-            {songInfo.songId}
-            {songInfo.songName}
-            {songInfo.youtubeLink}
-            {songInfo.genre}
-        </Container>
+        <>
+            {hidden ? "" :
+                <>
+                    {/* Song information */}
+                    {/* <td>{songInfo.songId}</td> */}
+                    <td>{songInfo.songName}</td>
+                    {/* <td>{songInfo.youtubeLink}</td> */}
+                    <td>{songInfo.genre}</td>
+                    <td style={{textAlign: "right"}}>
+                        <DropdownButton id="dropdown-basic-button" title="Options">
+                            <Dropdown.Item onClick={handleShow}>Add to Playlist</Dropdown.Item>
+                            <Dropdown.Item onClick={handleDelete}>Delete Song</Dropdown.Item>
+                        </DropdownButton>
+                    </td>
+
+
+                    {/* Modal for Adding to Playlist */}
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Add "{songInfo.songName}" to Playlist</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form.Select>
+                                <option disabled>Choose Playlist</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                                Add
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
+            }
+        </>
     )
 
 }
