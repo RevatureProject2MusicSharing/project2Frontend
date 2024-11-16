fbe67e0cdc1f6216d8534a4047b60cd6f7c2d22a
austinp0930 <58752431+austinp0930@users.noreply.github.com>
2024-11-16 15:18:43 -0600

    added functionality to log in button

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,15 +1,40 @@
-import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { Container, Form, Row } from "react-bootstrap"
 import {motion} from "motion/react"
 import "./Login.css"
 import { FcMusic } from "react-icons/fc";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login:React.FC = () => {
 
+const navigate = useNavigate()
+
+const[loginCreds, setLoginCreds] = useState({
+    username:"",
+    password:""
+})
+
+useEffect(() => {
+    
+    document.documentElement.style.setProperty('--bs-border-radius','5px')
+    
+},[])
 
 
+const storeValues = (input:any) => {
 
+    const name = input.target.name
+    const value = input.target.value
 
+    setLoginCreds((loginCreds) => ({...loginCreds, [name]:value}))
+    
 
+}
+
+const login = () => {
+
+    console.log(loginCreds)
+}
 
 return(
     
@@ -21,23 +46,34 @@ return(
         <Row>
             <Form>
                 <Form.Group>
-                    <Form.Label className = "LoginLabel">Username</Form.Label>
-                    <Form.Control className="formTextBox" type="text" placeholder ="Username"/>
+                    <Form.Label className = "LoginLabel mb-0">Username</Form.Label>
+                    <Form.Control className="mb-3" 
+                    type="text" 
+                    placeholder ="Username" 
+                    data-bs-theme = "dark"
+                    name="username"
+                    onChange={storeValues}/>
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label className ="LoginLabel">Password</Form.Label>
-                    <Form.Control className="formTextBox" type="password" placeholder = "Password"/>
+                    <Form.Label className ="LoginLabel mb-0">Password</Form.Label>
+                    <Form.Control className="" 
+                    type="password" 
+                    placeholder = "Password" 
+                    data-bs-theme = "dark"
+                    name="password"
+                    onChange={storeValues}/>
                 </Form.Group>
             </Form>
         </Row>
             <Row>
-            <motion.div
+            <motion.button
             className="box"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }
             }
-            >Log in</motion.div>
+            onClick={login}
+            >Log in</motion.button>
             </Row>
     </Container>
     
