6741a66870957ef043be3ee649bbca46720c02a4
austinp0930 <58752431+austinp0930@users.noreply.github.com>
2024-11-15 22:56:17 -0600

    finished skeleton of login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/Home'
+import { Login } from './Components/Login/Login'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
   return (
     <>
+    
       <BrowserRouter>
           <Routes>
-            <Route path="" element={<Home/>} /> 
+            <Route path="" element={<Login/>} /> 
           </Routes>
         </BrowserRouter>
     </>
--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,19 +1,49 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import {motion} from "motion/react"
+import "./Login.css"
+import { FcMusic } from "react-icons/fc";
+
 export const Login:React.FC = () => {
 
 
 
 
 
+
+
 return(
+    
+    <Container id="LoginBox">
+        <Row id="header">
+        <FcMusic id="icon" />
+        <h1 className = "LoginText" id="LoginHeader">Login</h1>
+        </Row>
+        <Row>
+            <Form>
+                <Form.Group>
+                    <Form.Label className = "LoginLabel">Username</Form.Label>
+                    <Form.Control className="formTextBox" type="text" placeholder ="Username"/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label className ="LoginLabel">Password</Form.Label>
+                    <Form.Control className="formTextBox" type="password" placeholder = "Password"/>
+                </Form.Group>
+            </Form>
+        </Row>
+            <Row>
+            <motion.div
+            className="box"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }
+            }
+            >Log in</motion.div>
+            </Row>
+    </Container>
+    
 
-
-    <>
     
     
-    
-    
-    </>
-
 )
 
 
