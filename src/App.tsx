263b2cd914be12b363e4b942b15c5fdd33cdba31
Brandon <bralew5377@gmail.com>
2024-11-18 13:39:55 -0500

    Users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './home/home'
+import Home from './Components/home/home'
 
 function App() {
 
---
+++ src/globalData/store.ts
@@ -0,0 +1,14 @@
+export const store:any = {
+
+
+
+
+
+
+
+
+
+
+
+    
+}
