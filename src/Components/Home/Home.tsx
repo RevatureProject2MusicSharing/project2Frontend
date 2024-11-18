294117f89749d3cf7a270b71e407a5e688a5ffc4
Joey <joeymhills@gmail.com>
2024-11-18 13:02:49 -0500

    just a small change on home screen

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
 import '../../App.css'
 
 function Home() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Welcome to the Home Page</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
       </div>
     </>
   )
