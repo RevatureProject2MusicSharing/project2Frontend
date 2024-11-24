9b0b0918cb8cab45a7a95da9d3dce978758607eb
Joshua-Vardeleon <Joshua.Vardeleon@gmail.com>
2024-11-24 02:32:45 -0600

    Refactored code to make card separate component

--- src/Components/home/home.tsx
+++ src/Components/home/home.tsx
@@ -5,78 +5,10 @@ import "./home.css"
 import { motion, useScroll, Variants } from 'motion/react'
 import { GiPokerHand } from "react-icons/gi";
 import { Button } from 'react-bootstrap';
+import { CardComponent } from './CardComponent';
 function Home() {
-  const [count, setCount] = useState(0)
-
-  interface Props {
-    desc: string;
-    hueA: number;
-    hueB: number;
-    textSize: number
-
-  }
-  
-  const cardVariants: Variants = {
-    offscreen: {
-      y: 300
-    },
-    onscreen: {
-      y: 50,
-      rotate: -10,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.8
-      }
-    }
-  };
-  
-  const hue = (h: number) => `hsl(${h}, 100%, 50%)`;
-  
-  function Card({desc, hueA, hueB, textSize}: Props) {
-    const background = ` linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
-  
-    return (
-      <motion.div
-        className="card-container"
-        initial="offscreen"
-        whileInView="onscreen"
-        viewport={{ once: true, amount: 0.8 }}
-      >
-        <div className="splash" style={{ background }} />
-        <motion.div className="card" variants={cardVariants} style={{fontSize:`${textSize}px`}} >
-          {desc}
-        </motion.div>
-      </motion.div>
-    );
-  }
-  
-  const food: [string, number, number, number][] = [
-    ["Features", 340, 10, 80],
-    ["Robust Login System", 20, 40, 50],
-    ["Secure Login Using JWT", 60, 90, 50],
-    ["Playing music via youtube", 80, 120, 50],
-    ["Custom Playlist Creation", 100, 140, 50],
-    ["Roulette Song Playing (Play Random Songs)", 205, 245, 45],
-  ];
-
-
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-
-
-{/* <div className='logo'>
-<GiPokerHand size={"50px"} className='wow'/>
-<p className='wow'>all in audio</p>
-
-</div> */}
 
       <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className='root'>
         <div>
@@ -95,10 +27,7 @@ function Home() {
         </div>
         
         <div>
-          <div className='hide' style={{ height: '400px', width: '500px', overflow: 'auto'}}>
-            {food.map(([desc, hueA, hueB, textSize]) => (
-            <Card desc={desc} hueA={hueA} hueB={hueB} key={desc} textSize={textSize} />))}
-          </div>
+          <CardComponent></CardComponent>
         </div>
       </motion.div>
 
