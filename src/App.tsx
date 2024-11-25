cbff91bc34c84c1f2a5bdd5ccbd1ae44d55b0acc
austinp0930 <58752431+austinp0930@users.noreply.github.com>
2024-11-25 12:32:50 -0600

    I love git

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Songs } from './Components/Songs/SongsPage'
-import Home from './Components/Home/home'
 import { Login } from './Components/Login/Login'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Register } from './Components/Login/Register/Register';
@@ -19,7 +18,6 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element ={<Register/>} />
-            <Route path="/" element={<Home/>}/>
             <Route path="/songs" element ={<PrivateRoute><Songs/></PrivateRoute>} />
             <Route path="/users" element = {<PrivateRoute roles={["Admin"]}><User/></PrivateRoute>}></Route>
           </Routes>
